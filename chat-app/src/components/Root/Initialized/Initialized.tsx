49562d97a3141dc6d0bea9926bd652adde87e5d7
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-03-29 21:32:19 -0700

    added toaster when username/password is invalid

--- chat-app/src/components/Root/Initialized/Initialized.tsx
+++ chat-app/src/components/Root/Initialized/Initialized.tsx
@@ -46,4 +46,5 @@ const Initialized = () => {
     </Routes>
   );
 };
+
 export default Initialized;
--- chat-app/src/components/Root/Initialized/Login/Login.tsx
+++ chat-app/src/components/Root/Initialized/Login/Login.tsx
@@ -1,5 +1,17 @@
-import { Card, Classes, Elevation, FormGroup, InputGroup } from "@blueprintjs/core";
+import { gql, useMutation } from "@apollo/client";
+import { Button, Card, Classes, Elevation, FormGroup, InputGroup, Intent } from "@blueprintjs/core";
+import { Controller, useForm } from "react-hook-form";
 import styled from "styled-components";
+import { useRecoilState } from "recoil";
+
+import userSessionAtom from "#root/recoil/atoms/userSession";
+import useGenerateId from "#utils/hooks/forms/useGenerateId";
+import toaster from "#utils/misc/toaster";
+
+interface FormData {
+  username: string;
+  password: string;
+}
 
 const Form = styled.form`
   margin: auto;
@@ -26,21 +38,91 @@ const Wrapper = styled.div`
   width: 100%;
 `;
 
+const mutation = gql`
+  mutation($password: String!, $username: String!) {
+    createUserSession(
+      password: $password
+      username: $username
+    ) {
+      user {
+        username
+      }
+    }
+  }
+`;
+
 const Login = () => {
-    return (
-      <Wrapper>
-        <Form> 
-          <Card elevation={Elevation.TWO}>
-            <Heading> Chat App </Heading>
-            <LargeFormGroup label="Username">
-              <InputGroup autoFocus large />
-            </LargeFormGroup>
-            <LargeFormGroup label="Password">
-              <InputGroup autoFocus large type="password" />
-            </LargeFormGroup>
-          </Card>
-        </Form>
-      </Wrapper>
-    );
+  const { 
+    control,
+    handleSubmit, 
+  } = useForm<FormData>();
+  const [createUserSession] = useMutation(mutation);
+  const generateId = useGenerateId();
+  const [, setUserSession] = useRecoilState(userSessionAtom);
+
+  const onSubmit = async ({ username, password }: FormData) => {
+    try {
+    const result = await createUserSession({ variables: { password, username}});
+    console.log(result);
+    } catch(err) {
+      toaster.show({ intent: Intent.DANGER, message: "Something went wrong! Please try again." });
+    }
   };
-  export default Login;
+
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit(onSubmit)}> 
+        <Card elevation={Elevation.TWO}>
+          <Heading> Chat App </Heading>
+          <LargeFormGroup label="Username" labelFor={generateId("username")}>
+            <Controller 
+              control={control}
+              name="username"
+              render={({
+                field: { onChange, onBlur, ref, value },
+              }) => (
+                <InputGroup 
+                  defaultValue={value}
+                  id={generateId("username")} 
+                  inputRef={ref}
+                  large 
+                  onBlur={onBlur}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </LargeFormGroup>
+          <LargeFormGroup label="Password" labelFor={generateId("password")}>
+            <Controller
+              control={control}
+              name="password"
+              render={({
+                field: { onChange, onBlur, ref, value },
+              }) => (
+                <InputGroup 
+                  defaultValue={value}
+                  id={generateId("password")}
+                  inputRef={ref}
+                  large
+                  onBlur={onBlur}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </LargeFormGroup>
+          <Button intent={Intent.PRIMARY} large type="submit">
+            Login
+          </Button>
+        </Card>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Login;
+
+/*
+<LargeFormGroup label="Password" labelFor={generateId("password")}>
+            <InputGroup name="password" id={generateId("password")} large type="password" {...register("password"), { required: true }} />
+          </LargeFormGroup>
+*/
